26322941f8f1beb24d6f9e6aa4e01d0fde770fce
stuv140 <stuv.otus@yandex.ru>
2026-02-28 20:15:02 +0300

    Add files via upload

---
+++ include/Circle.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "GraphicPrimitive.cpp"
+//#include "Line.h"
+
+class Circle: public GraphicPrimitive
+{
+private:
+    Point center_;
+    int radius_;
+
+public:
+    Circle(Point center, int radius) : center_(center), radius_(radius) {}
+    void draw() const override;
+    Point GetCenter()const { return center_; }
+    int GetRadius()const { return radius_; }
+};
+
---
+++ include/GraphicPrimitive.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+struct Point {
+    int x, y;
+};
+class GraphicPrimitive {
+public:
+    virtual ~GraphicPrimitive() {}
+    virtual void draw() const = 0;  
+};
+
