f62d75ce2b3a722261feb4579a2e54d722cfc45c
stuv140 <stuv.otus@yandex.ru>
2026-02-28 20:10:04 +0300

    Add files via upload

---
+++ src/Line.cpp
@@ -0,0 +1,7 @@
+﻿#pragma once
+#include "Line.h"
+#include<iostream>
+
+void Line::draw()const {
+	std::cout << "Draw Line: Begin point (" << begin.x << "," << begin.y << ") End point (" << end.x << "," << end.y << ")" << std::endl;
+}
