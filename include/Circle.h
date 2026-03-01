898c0031c8f9acfd865b5bde536bfc083d57c57b
stuv140 <stuv.otus@yandex.ru>
2026-03-01 11:25:35 +0300

    Update Circle.h

--- include/Circle.h
+++ include/Circle.h
@@ -11,11 +11,13 @@ private:
 
 public:
     Circle(Point center, int radius) : center_(center), radius_(radius) {}
+    Circle():center_{},radius_{1} {}
     void draw() const override;
     Point GetCenter()const { return center_; }
     int GetRadius()const { return radius_; }
     virtual ~Circle() {};
 };
+
 
 
 
