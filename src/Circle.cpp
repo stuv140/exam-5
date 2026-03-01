532ccf7ded3120ef5bbd5fa461aa0db364835873
stuv140 <stuv.otus@yandex.ru>
2026-03-01 11:27:35 +0300

    Update Circle.cpp

--- src/Circle.cpp
+++ src/Circle.cpp
@@ -1,7 +1,8 @@
-﻿#pragma once
+
 #include "Circle.h"
 #include <iostream>
 
 void Circle::draw() const {
 	std::cout << "Draw circle with centre (" << center_.x << "," << center_.y << ") radius = " << radius_<<std::endl;
+
 }
