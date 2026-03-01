8903a9478c77b2e6d10f40131fc044b77f7f1982
stuv140 <stuv.otus@yandex.ru>
2026-03-01 11:28:26 +0300

    Update Line.cpp

--- src/Line.cpp
+++ src/Line.cpp
@@ -1,7 +1,8 @@
-﻿#pragma once
+
 #include "Line.h"
 #include<iostream>
 
 void Line::draw()const {
 	std::cout << "Draw Line: Begin point (" << begin.x << "," << begin.y << ") End point (" << end.x << "," << end.y << ")" << std::endl;
+
 }
